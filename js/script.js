2e558ea71eb7696abc4715585c172afd7fcafa1d
Ilia <qqwondeful@gmail.com>
2023-03-26 15:58:42 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+alert('Zalupa ti tut?');
+
+let name = confirm('Your name is Ilia?', '');
+
+
+let isMarried = prompt('Are you married?', '');
+
+alert(`ebat ti loh, if your answer is ${isMarried}`);

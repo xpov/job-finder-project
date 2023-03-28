e1df9a549636569215cf9351485095eecc9a302b
Ilia <qqwondeful@gmail.com>
2023-03-27 22:15:51 -0400

    all files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 "use strict";
 
-alert('Zalupa ti tut?');
 
-let name = confirm('Your name is Ilia?', '');
+const numberOffilms = +prompt('How much films did you watch?', '');
+
+const personalMovieDB = {
+    count: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('What is the last film that did you watch?', ''),
+      b = prompt('How you can estimate its?', ''),
+      d = prompt('What is the last film that did you watch?', ''),
+      c = prompt('How you can estimate its?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 
-let isMarried = prompt('Are you married?', '');
+console.log(personalMovieDB);
 
-alert(`ebat ti loh, if your answer is ${isMarried}`);
+
+
